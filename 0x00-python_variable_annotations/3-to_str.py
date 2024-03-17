786e2eec7a4efe9c9a0d6780d86b02648e17b343
Celeekeybor <celestinbor02@gmail.com>
2024-03-17 01:26:27 -0700

    initial commit

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""Basic annotations - floor"""
+
+
+def to_str(n: float) -> str:
+    """Takes a float n as argument and returns the string rep of float."""
+    return str(n)
