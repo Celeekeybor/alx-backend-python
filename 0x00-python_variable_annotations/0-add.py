c35a255ecbba1758adc73def49605a776833dda0
Celeekeybor <celestinbor02@gmail.com>
2024-03-17 01:20:10 -0700

    add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""Function that adds two float numbers."""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Add two floats.
+
+    Parameters:
+    a (float): The first float.
+    b (float): The second float.
+
+    Returns:
+    float: The sum of the two floats.
+    """
+    return a + b
