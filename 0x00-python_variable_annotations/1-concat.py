b776bd6323e599f8b0e5537ff9139ef2140fb620
Celeekeybor <celestinbor02@gmail.com>
2024-03-17 01:23:04 -0700

    concatenate

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""Function to concatenate two strings with a space in between."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenate two strings with a space in between.
+
+    Parameters:
+    str1 (str): The first string.
+    str2 (str): The second string.
+
+    Returns:
+    str: The concatenated string.
+    """
+    return str1+str2
